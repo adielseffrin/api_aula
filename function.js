bdc492d0a40b54d893825acad14eb1695a3ec423
Adiel Seffrin <adielseffrin@gmail.com>
2020-10-06 20:08:45 -0300

    envio de requisições pra nasa

--- function.js
+++ function.js
@@ -1,20 +1,30 @@
-function enviarRequisicao(){
+function consultarImagem(){
+  
   var request = new XMLHttpRequest();
-  var opcao = document.getElementById("opcao").value;
-  var url = "";
+  
+  var dataImagem = document.getElementById("data-imagem").value;
+  console.log(dataImagem);
+  
+  var url = "https://api.nasa.gov/planetary/apod?api_key=xxxx";
+  
+  if(dataImagem != "") url += "&date="+dataImagem;
 
-  request.open("GET", url, true);
-
+//request.open("GET","  https://pokeapi.co/api/v2/pokemon/"+cnpj,true);
+  
+  request.open("GET",url,true);
+  
   request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      let response = JSON.parse(this.responseText); //o que faz isso aqui????
-      console.log(response); //e isso aqui também?
+      let response = JSON.parse(this.responseText);
+      console.log(response);
 
-     
+      let html = "<p>"+response.title+"</p>";
+      html +="<img src='"+response.url+"' alt='"+response.title+"'/>";
+
+
+      document.getElementById("resultado").innerHTML = html;
     }
   };
 
   request.send();
-
-
 }
